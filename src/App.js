ec47a39f9bd9fdba95302dc7045a577771682d89
Giorgi-Tabatadze <giorgi.taba@icloud.com>
2022-08-27 16:49:12 +0400

    add router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import levelsArray from "./components/levelSelector/levels";
+import Header from "./components/header/header";
+import LevelSelector from "./components/levelSelector/selector";
+import Game from "./components/game/game";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <Router>
+      <Header levels={levelsArray} />
+      <Routes>
+        <Route path="/" element={<LevelSelector levels={levelsArray} />} />
+        <Route path="/leaderboards" element={<div>This is Leaderboard</div>} />
+        <Route path="/game/:gameId" element={<Game levels={levelsArray} />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
